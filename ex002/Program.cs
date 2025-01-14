bef7ac6d591d31ceb04672998cda2f8f8c67ce3e
Alinne <a-linnejs@hotmail.com>
2025-01-13 22:06:37 -0300

    Adicionar arquivos de projeto.

---
+++ ex002/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ex002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 10;
+            float num = 3.14f; 
+            char letra = 'A';
+            string nome = "Alinne";
+
+
+            Console.WriteLine("O número é {0}", numero);
+            Console.WriteLine("O valor é R${0}...", num);
+            Console.WriteLine("A primeira letra do meu nome é {0}", letra);
+            Console.WriteLine("Eu me chamo {0}!", nome);
+
+            Console.WriteLine("Saí com {0} reais de casa, e o valor do meu chocolate foi {1}! A primeira letra do meu nome é {2} - me chamo {3}.", numero, num, letra, nome);  
+
+        }
+    }
+}
---
+++ ex005/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ex005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+        
+
+            Console.WriteLine("Digite um número:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+    
+            Console.WriteLine("{0} * 1 = {1}", num1, num1*1);
+            Console.WriteLine("{0} * 2 = {1}", num1, num1 * 2);
+            Console.WriteLine("{0} * 3 = {1}", num1, num1 * 3);
+            Console.WriteLine("{0} * 4 = {1}", num1, num1 * 4);
+            Console.WriteLine("{0} * 5 = {1}", num1, num1 * 5);
+            Console.WriteLine("{0} * 6 = {1}", num1, num1 * 6);
+            Console.WriteLine("{0} * 7 = {1}", num1, num1 * 7);
+            Console.WriteLine("{0} * 8 = {1}", num1, num1 * 8);
+            Console.WriteLine("{0} * 9 = {1}", num1, num1 * 9);
+            Console.WriteLine("{0} * 10 = {1}", num1, num1 * 10);
+
+
+        }
+    }
+}

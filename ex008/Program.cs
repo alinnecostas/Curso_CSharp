44dc86fe807c92e09ebc23f30dd0514df2f6009f
Alinne <a-linnejs@hotmail.com>
2025-01-15 22:25:11 -0300

    Exercicio 1

---
+++ ex008/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double limite_pista;
+            double km_rodado;
+            string resultado;
+
+            limite_pista = 80;
+
+            Console.Write("Km rodados: ");
+            km_rodado = (Convert.ToDouble(Console.ReadLine()));
+
+
+            limite_pista = (km_rodado - limite_pista);
+
+            if (km_rodado <= 80)
+            {
+                Console.WriteLine("Não foi localizado multa!");
+            }
+
+
+            else
+            {
+                resultado = "Multado!";
+
+                Console.WriteLine("{1} A multa custará: R${0},00!", limite_pista * 7, resultado);
+
+            }
+
+
+
+        }
+    }
+}

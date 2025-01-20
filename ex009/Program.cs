8d2228d981144520b0f99d57a8ec901be906dabb
Alinne <a-linnejs@hotmail.com>
2025-01-20 19:41:00 -0300

    Alterações de pastas.

---
+++ ex009/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ex009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //impar ou par
+
+            int numero;
+
+            Console.Write("Digite um numero: ");
+            numero = (Convert.ToInt32(Console.ReadLine()));
+
+            numero = (numero % 2);
+
+            if (numero == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+
+        }
+    }
+}

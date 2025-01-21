8ac1173eb9431961e8c35e3f03fa4d1d61106c1d
Alinne <a-linnejs@hotmail.com>
2025-01-20 22:24:12 -0300

    Exercicio feito.

--- TesteTriangulo/Program.cs
+++ TesteTriangulo/Program.cs
@@ -4,6 +4,28 @@ namespace TesteTriangulo
 {
     internal class Program
     {
+        /* static void Main(string[] args)
+         {
+             int ladoA, ladoB, ladoC;
+
+             Console.Write("Digite o valor do lado A: ");
+             ladoA = Convert.ToInt32(Console.ReadLine());
+
+             Console.Write("Digite o valor do lado B: ");
+             ladoB = Convert.ToInt32(Console.ReadLine());
+
+             Console.Write("Digite o valor do lado C: ");
+             ladoC = Convert.ToInt32(Console.ReadLine());
+
+             if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoB))
+             {
+                 Console.WriteLine("Os valores informados formam um triangulo.");
+             }
+             else
+             {
+                 Console.WriteLine("Os valores informados não formam um triangulo.");
+             }*/
+
         static void Main(string[] args)
         {
             int ladoA, ladoB, ladoC;
@@ -20,12 +42,20 @@ namespace TesteTriangulo
             if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoB))
             {
                 Console.WriteLine("Os valores informados formam um triangulo.");
+                
+                if ((ladoA == ladoB) && (ladoB == ladoC))
+                {
+                    Console.WriteLine("Ele é um triangulo equilátero.");    
+                }
+                else if ((ladoA == ladoB) || (ladoA == ladoC) || (ladoB == ladoC))
+                {
+                    Console.WriteLine("Ele é um triangulo isósceles.");
+                }
             }
             else
             {
-                Console.WriteLine("Os valores informados não formam um triangulo.");
+                Console.WriteLine("Ele é um triangulo escaleno.");
             }
-            
         }
     }
 }
---
+++ ex11/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sal1;
+
+
+            Console.WriteLine("Digite seu salário:");
+            sal1 = Convert.ToDouble(Console.ReadLine());
+
+ 
+
+            if (sal1 > 2500)
+            {
+                Console.WriteLine("Seu salário é mais 5%: {0}, sendo o total de {1}", sal1, sal1 * 0.05 + sal1);
+            }
+            else if (sal1 > 1250)
+            {
+                Console.WriteLine("Seu salário é mais 10%: {0}, sendo o total de {1}", sal1, sal1 * 0.10 + sal1);
+            }
+            else 
+            {
+                Console.WriteLine("Seu salário é mais 15%: {0}, sendo o total de {1}", sal1, sal1 * 0.15 + sal1);
+            }
+
+        }
+    }
+}

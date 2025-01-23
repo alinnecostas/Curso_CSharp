5110df01ac3a96c23aab19978f8590a62c1f033c
Alinne <a-linnejs@hotmail.com>
2025-01-22 21:58:36 -0300

    Exercicio while!

---
+++ ex15/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ex15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string genero;
+
+            Console.Write("Digite o gênero: ");
+            genero = Console.ReadLine().ToUpper();
+            
+            while (genero != "F" && genero != "M") 
+            {
+                Console.Write("Digite novamente o gênero: ");
+                genero = Console.ReadLine().ToUpper();
+            }
+
+            Console.WriteLine("Bem-vindo ao curso de C#!");
+        }
+       
+    }
+}

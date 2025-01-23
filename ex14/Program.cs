9f01865cbcd6482b108c2a77c58e18a81f256d8e
Alinne <a-linnejs@hotmail.com>
2025-01-22 21:36:19 -0300

    Atividade tabuada com "for"

---
+++ ex14/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ex14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int tabuada;
+
+            Console.WriteLine("Digite um numero: ");
+            tabuada = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <=10; i++)
+          
+                {
+                    Console.WriteLine("{0} x {1} = {2}", tabuada, i, tabuada*i);
+                }
+        }
+    }
+}

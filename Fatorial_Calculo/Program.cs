af1f2672aefa991f4ef7484c1b6641ba27a7e820
Alinne <a-linnejs@hotmail.com>
2025-02-03 21:00:46 -0300

    Renomeação dos exercicios

---
+++ Fatorial_Calculo/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Fatorial_Calculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fatorial(5));
+        }
+
+        static int fatorial(int n, bool show = false)
+        {
+            int f = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (show)
+                {
+                    Console.WriteLine(i);
+                    if (i < n)
+                    {
+                        Console.Write(" x ");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" = ");
+                    }
+                }
+
+                f *= i;
+            }
+
+            return f;
+
+        }
+    }
+}
